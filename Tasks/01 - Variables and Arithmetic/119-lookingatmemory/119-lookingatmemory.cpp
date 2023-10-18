b4f9a6c4ed9cc960dc3ed63ad1cf075772b28638
kjaquino1 <kelvin.aquino@students.plymouth.ac.uk>
2023-10-18 19:16:57 +0100

    117 & 119

--- Tasks/01 - Variables and Arithmetic/119-lookingatmemory/119-lookingatmemory.cpp
+++ Tasks/01 - Variables and Arithmetic/119-lookingatmemory/119-lookingatmemory.cpp
@@ -8,7 +8,7 @@ int main()
     unsigned int myVariable;
     myVariable = 15;    // 000F in Hex, 0000000011111111 in binary
     
-    printf("The value of myVariable is: %d\n", myVariable);
+     printf("The value of myVariable is: %d\n", myVariable);
     printf("The value of myVariable in base 16 (hex) is: %x\n", myVariable);
 
     printf("The address (in hex) of myVariable is %p\n", &myVariable);
